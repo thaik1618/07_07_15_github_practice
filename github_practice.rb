2a218408bd7608e534588f96a6609e00651a2127
cndmnt <noah@cndmnt.com>
2015-07-07 00:30:23 +0000

    make changes to favsong to practice with git

--- github_practice.rb
+++ github_practice.rb
@@ -1,22 +1,33 @@
-#This is meant to give you a bit of practice with GitHub
+# This is meant to give you a bit of practice with GitHub
+# Run the file by typing "ruby github_practice.rb" in your command line.
+
+puts "Dear developer,"
+puts ""
+
+#Please change the following variable to one of your favorite songs
+favsong = "Straight From the Harp by Damu the Fudgemunk"
+
+#Feel free to change your feelings as well.
+feeling = "enthused"
+
+puts "Whenever I'm writing code, I always love to listen to #{favsong}."
+puts "I don't know why... I guess it just makes me feel #{feeling}."
 
 # STEP  1: Find this repository on GitHub. (You probably already did this, since you're reading this message.)
 # STEP  2: Fork this repository onto your GitHub account. (Click the Fork button in the upper right corner)
 # STEP  3: Copy the HTTPS link to your clipboard to clone the repo
 # STEP  4: Type "git clone [paste_the_https_url_in_git_hub]" and hit enter
 # STEP  5: Make changes to the github_practice.rb file
+
+#  WOO!    IMPRESSIVE!
+#  WOO!    Do you want more? See if you can follow these instructions for git on your own.
+
 # STEP  6: Make sure you are in the 07_07_15_github_practice directory (Remember how to print working directory?)
 # STEP  7: Type "git status" and hit enter (You should see one untracked file)
 # STEP  8: Type "git add github_practice.rb" and hit enter (This will prepare the file to be pushed to GitHub)
-# STEP  9: Type "git commit -m "Add fav song" and hit enter (This will add a brief note and a save marker to git)
+# STEP  9: Type "git commit -m "my first commit" and hit enter (This will add a note and a save to git)
 # STEP 10: Type "git push origin master"
+# STEP 11: Check the repo that you forked on GitHub.
+# STEP 12: Woo! Wooo! Woooooo!
 
-puts "Dear developer,"
-
-#Please change the following variable to one of your favorite songs
-favsong = ""
-
-#Feel free to change your feelings as well.
-feeling = "enthused"
-
-puts "Whenever I'm writing code, I always love to listen to #{favsong}. I don't know why. I guess it just makes me feel #{feeling}."
+#          You are level 2 git masta'!
