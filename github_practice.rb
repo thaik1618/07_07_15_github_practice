62a0b542d620cbd538d641b7e30d953d5008c16f
Noah Gordon <noah@cndmnt.com>
2015-07-06 20:40:27 -0400

    Update github_practice.rb

--- github_practice.rb
+++ github_practice.rb
@@ -1,8 +1,10 @@
-# This is meant to give you a bit of practice with GitHub
-# Run the file by typing "ruby github_practice.rb" in your command line.
+# 07_07_15_github_practice
 
-puts "Dear developer,"
-puts ""
+# This is meant to give you a bit of practice with GitHub. You will practice cloning, forking, making changes, and pushing your changes on GitHub.
+# Start by running the file. (You run a ruby file by typing "ruby github_practice.rb" in your command line.)
+
+# Once you understand how the program works, please change the variable #favsong to one of your favorite songs. Feel 
+# free to change your feelings as well. Finally, save your changes.
 
 #Please change the following variable to one of your favorite songs
 favsong = "Straight From the Harp by Damu the Fudgemunk"
@@ -13,21 +15,19 @@ feeling = "enthused"
 puts "Whenever I'm writing code, I always love to listen to #{favsong}."
 puts "I don't know why... I guess it just makes me feel #{feeling}."
 
-# STEP  1: Find this repository on GitHub. (You probably already did this, since you're reading this message.)
-# STEP  2: Fork this repository onto your GitHub account. (Click the Fork button in the upper right corner)
-# STEP  3: Copy the HTTPS link to your clipboard to clone the repo
-# STEP  4: Type "git clone [paste_the_https_url_in_git_hub]" and hit enter
-# STEP  5: Make changes to the github_practice.rb file
 
-#  WOO!    IMPRESSIVE!
-#  WOO!    Do you want more? See if you can follow these instructions for git on your own.
 
-# STEP  6: Make sure you are in the 07_07_15_github_practice directory (Remember how to print working directory?)
-# STEP  7: Type "git status" and hit enter (You should see one untracked file)
-# STEP  8: Type "git add github_practice.rb" and hit enter (This will prepare the file to be pushed to GitHub)
-# STEP  9: Type "git commit -m "my first commit" and hit enter (This will add a note and a save to git)
-# STEP 10: Type "git push origin master"
-# STEP 11: Check the repo that you forked on GitHub.
-# STEP 12: Woo! Wooo! Woooooo!
 
-#          You just leveled up your gitness. My goodness, you're good!
+###
+
+# LOOKING TO STRETCH?
+
+# Now that you've completed the practice. You can practice your git skills by uploading your new files to GitHub. 
+
+# STEP 1: Make sure you are in the 07_07_15_github_practice directory (Remember how to print working directory?)
+# STEP 2: Type "git status" and hit enter (You should see one untracked file)
+# STEP 3: Type "git add github_practice.rb" and hit enter (This will prepare the file to be pushed to GitHub)
+# STEP 4: Type "git commit -m "my first commit" and hit enter (This will add a note and a save to git)
+# STEP 5: Type "git push origin master" (This pushes your changed files back up to GitHub)
+# STEP 6: Check the repo that you forked on GitHub. (You should see your changed file and your comment.
+# STEP 7: Woo! Wooo! Woooooo! You just leveled up your gitness. My goodness, you're good!
